1ac18d9520417f2e1efdeaa3623e85401a090460
Philipp Parzer <phil0898.pp@gmail.com>
2021-09-06 03:51:29 +0200

    updated stylesheet, added hrefs

---
+++ localization.js
@@ -0,0 +1,42 @@
+//handles localization on button click
+
+const switchButton = (langparam) => {
+
+    
+    LANG_BTNS.forEach(element => element.classList = "col-10 language-links")
+    
+    switch(langparam)
+    {
+        case 'en':
+            EN_BTN.classList = "col-10 language-links doc-language";
+            break;
+        case 'ru':
+            RU_BTN.classList = "col-10 language-links doc-language";
+            break;
+        case 'de':
+            DE_BTN.classList = "col-10 language-links doc-language";
+            break;
+        case 'fr':
+            FR_BTN.classList = "col-10 language-links doc-language";
+            break;
+        default: 
+            console.log("language error")
+    }
+}
+
+const switchText = (langparam) => {
+    TEXT_ELES.forEach(element => {
+        element.textContent = TEXT_HREFS[element.id][langparam][0];
+        /*href of element = TEXT_HREFS[element.id][langparam][1];*/
+    })
+}
+
+const switchLang = (langparam) => {
+    
+    document.documentElement.setAttribute('lang', langparam);
+
+    switchButton(langparam);
+    switchText(langparam);
+
+
+}
